c110b3a67019ac3399982eaae10de861cb3875a4
Mattias Karlsson <matkar@wcom.se>
2025-11-06 16:10:50 +0100

    Initial Cake.cs

---
+++ cake.cs
@@ -0,0 +1,33 @@
+#:sdk Cake.Sdk
+
+Task("Restore")
+    .Does(()=>DotNetRestore("src"));
+
+Task("Build")
+    .IsDependentOn("Restore")
+    .Does(()=>DotNetBuild("src"));
+
+Task("Test")
+    .IsDependentOn("Build")
+    .Does(()=>DotNetTest("src"));
+
+Task("Pack")
+    .IsDependentOn("Test")
+    .Does(()=>DotNetPack("src",
+    new DotNetPackSettings
+    {
+        OutputDirectory ="./artifacts"
+    }));
+
+Task("UploadArtifact")
+    .IsDependentOn("Pack")
+    .WithCriteria(GitHubActions.IsRunningOnGitHubActions)
+    .Does(()=>GitHubActions.Commands.UploadArtifact(
+        Directory("./artifacts"),
+        "NuGet"
+    ));
+
+Task("GitHubActions")
+    .IsDependentOn("UploadArtifact");
+
+RunTarget(Argument("target", "Test"));
